8772ce3831c22498c025ce29b8e806d3bd3950ac
jhoneiro <jhoneiro12@hotmail.com>
2024-06-12 13:26:20 -0500

    registrar evento cors configurado

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -25,6 +25,40 @@ resource "aws_api_gateway_integration" "registrar_evento" {
   uri                     = aws_lambda_function.registrar_evento.invoke_arn
 }
 
+resource "aws_api_gateway_method_response" "registrar_evento_200" {
+  rest_api_id = aws_api_gateway_rest_api.evento_api.id
+  resource_id = aws_api_gateway_resource.evento.id
+  http_method = aws_api_gateway_method.registrar_evento.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "registrar_evento_200" {
+  rest_api_id = aws_api_gateway_rest_api.evento_api.id
+  resource_id = aws_api_gateway_resource.evento.id
+  http_method = aws_api_gateway_method.registrar_evento.http_method
+  status_code = aws_api_gateway_method_response.registrar_evento_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.registrar_evento
+  ]
+}
+
 resource "aws_lambda_permission" "registrar_evento_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
