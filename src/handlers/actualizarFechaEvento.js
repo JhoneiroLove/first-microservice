ed26e11a9f06d79aaf442c2a329951ea06e14288
jhoneiro <jhoneiro12@hotmail.com>
2024-06-22 12:10:35 -0500

    lambda de actualizar fecha coregido, activado

--- src/handlers/actualizarFechaEvento.js
+++ src/handlers/actualizarFechaEvento.js
@@ -1,12 +1,32 @@
 const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const jwt = require('jsonwebtoken');
 
 exports.handler = async (event) => {
-    const { eventoId, nuevaFecha } = JSON.parse(event.body);
+    console.log('Event:', JSON.stringify(event));
+    
+    const token = event.headers.Authorization.replace("Bearer ", "");
 
+    let userId;
+    try {
+        const decoded = jwt.decode(token);
+        userId = decoded.userId.toString(); 
+    } catch (err) {
+        console.error('Error decoding token:', err);
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'Invalid token' })
+        };
+    }
+
+    const { eventoId, nuevaFecha } = JSON.parse(event.body);
+    
     const params = {
         TableName: process.env.EVENTOS_TABLE,
-        Key: { ID: eventoId },
+        Key: {
+            UserId: userId,
+            EventoId: eventoId
+        },
         UpdateExpression: 'set Fecha = :nuevaFecha, ModificadoFecha = :modificadoFecha',
         ExpressionAttributeValues: {
             ':nuevaFecha': nuevaFecha,
@@ -17,11 +37,13 @@ exports.handler = async (event) => {
 
     try {
         const result = await dynamodb.update(params).promise();
+        console.log('Update result:', result);
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'Fecha de evento actualizada', result }),
         };
     } catch (error) {
+        console.error('Error updating event:', error);
         return {
             statusCode: 500,
             body: JSON.stringify({ message: 'Error al actualizar la fecha del evento', error }),
