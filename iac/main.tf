79fee76553e219bce9a95ad7d63d8408e105ccd4
jhoneiro <jhoneiro12@hotmail.com>
2024-06-28 03:07:20 -0500

    deploy CI/CD

--- iac/main.tf
+++ iac/main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region      = var.aws_region
-  profile     = var.aws_role[local.env_name]
+  access_key  = var.aws_access_key
+  secret_key  = var.aws_secret_key
   max_retries = 2
 
   default_tags {
--- iac/variables.tf
+++ iac/variables.tf
@@ -4,6 +4,16 @@ variable "aws_role" {
   type        = map(string)
 }
 
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+
 variable "aws_region" {
   description = "AWS Region"
   type        = string
